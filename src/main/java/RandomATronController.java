55dd38dd1dfca2d028995f7d230c457e731132a6
Brian Cooke <brian.cooke@live.co.uk>
2018-05-22 10:55:20 +0100

    Minor refactoring

--- src/main/java/RandomATronController.java
+++ src/main/java/RandomATronController.java
@@ -3,6 +3,7 @@ import spark.template.velocity.VelocityTemplateEngine;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 import static spark.Spark.get;
@@ -16,30 +17,10 @@ public class RandomATronController {
         VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
         staticFileLocation("/public");
 
-        ArrayList<String> students = new ArrayList<String>();
-        students.add("Paul");
-        students.add("Andrew");
-        students.add("Patrick");
-        students.add("Daniel");
-        students.add("Sarah");
-        students.add("Kelsie");
-        students.add("Liam");
-        students.add("Ed");
-        students.add("Scott");
-        students.add("Lyle");
-        students.add("Stuart");
-        students.add("Connor");
-        students.add("Fraser");
-        students.add("Callum");
-        students.add("Chris");
-        students.add("Ruaridh");
-        students.add("Nikhil");
-        students.add("Roberto");
-        students.add("Brian");
+        ArrayList<String> students = new ArrayList<>();
+        Collections.addAll(students, "Paul", "Andrew", "Patrick", "Daniel", "Sarah", "Kelsie", "Liam", "Ed", "Scott", "Lyle", "Stuart", "Connor", "Fraser", "Callum", "Chris", "Ruaridh", "Nikhil", "Roberto", "Brian");
 
         RandomATron randomATron = new RandomATron(students);
-
-        ArrayList<String> randomStudents = randomATron.randomPair();
 
 
         get("/randomatron", (req, res) -> {
@@ -60,7 +41,7 @@ public class RandomATronController {
 
         get("/two", (req, res) -> {
             HashMap<String, Object> model = new HashMap<>();
-            model.put("pair1", randomATron.randomPair());
+            model.put("pair", randomATron.randomPair());
             model.put("template", "two.vtl");
             return new ModelAndView(model, "layout.vtl");
         }, velocityTemplateEngine);
